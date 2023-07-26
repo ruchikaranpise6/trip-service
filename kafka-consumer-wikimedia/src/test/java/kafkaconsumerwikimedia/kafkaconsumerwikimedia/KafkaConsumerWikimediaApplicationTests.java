9e79dd691e4f88d05fd21d9fb4022bf03881c4e1
rranpise <rranpise@calibo.com>
2023-07-27 00:38:22 +0530

    Adding Kafka project

---
+++ kafka-consumer-wikimedia/src/test/java/kafkaconsumerwikimedia/kafkaconsumerwikimedia/KafkaConsumerWikimediaApplicationTests.java
@@ -0,0 +1,13 @@
+package kafkaconsumerwikimedia.kafkaconsumerwikimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerWikimediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ kafka-producer-wikimedia/src/test/java/kafkaproducerwikimedia/kafkaproducerwikimedia/KafkaProducerWikimediaApplicationTests.java
@@ -0,0 +1,13 @@
+package kafkaproducerwikimedia.kafkaproducerwikimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerWikimediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
